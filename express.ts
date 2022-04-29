2a98628b04bd997d4e057f1265b572adb97eebf0
WatsBart <watsbart@gmail.com>
2022-04-29 11:27:25 +0200

    Bekijken lego figuur uit database

--- express.ts
+++ express.ts
@@ -12,7 +12,6 @@ app.use(express.static('views'));
 
 var db_data: any;
 var figdata: any;
-var db_dataBekijken: any;
 
 var db = mysql.createConnection({
     host: 'db4free.net',
@@ -27,62 +26,49 @@ db.connect((err: any) => {
     console.log('Database is connected successfully');
 });
 
-db.query("select * from `Bekijken`", (err: any, results: any) => {
-    if (err) console.log("can't connect");
-    db_data = results;
-    let figIds = db_data[0].fig_id.toString();
-//     for (let teller = 0; teller<figIds.length;teller++) {
-//         let dezeId = figIds[teller].toString();
-//     for (var i = dezeId.length; i < 6; i++) {
-//         dezeId = "0" + dezeId;
-//     }
-//     figIds[teller] = dezeId;
-// }
-    console.log(figIds);
-    //for (let i = 0; i<figIds.length; i++) {
-        db_dataBekijken = axios.get(`https://rebrickable.com/api/v3/lego/minifigs/fig-${figIds}/?key=3ef36135e7fda4370a11fd6191fef2af`).
-        then(function (response: any) {
-            console.log(response);
-            return response.data;
-        })
-        
-    //}
-})
 
-// db.query("select * from `Bekijken`", (err: any, results: any) => {
-//     if (err) console.log("can't connect");
-//     db_dataBekijken = results;
-//     // let bekijkenHtml = document.getElementById('bekijkenTabel');
-//     // bekijkenHtml?.insertAdjacentHTML('beforeend', `<tr>
-//     // <td><img src="${db_dataBekijken[0].afbeelding_minifig}"></td>
-//     // <td>${db_dataBekijken[0].code_minifig}</td>
-//     // <td><img src="${db_dataBekijken[0].afbeelding_set}"></td>
-//     // <td>${db_dataBekijken[0].code_set}</td>
-//     // </tr>`);
-//     // bekijkenHtml?.insertAdjacentHTML('beforeend', "</table>");
-// })
+
 
 app.get('/', (req: any, res: any) => {
     res.render('index')
 });
 
-app.get('/index.html', (req: any, res: any) => {
+app.get('/index', (req: any, res: any) => {
     res.render('index');
 })
 
-app.get('/informatie.html', (req: any, res: any) => {
+app.get('/informatie', (req: any, res: any) => {
     res.render('informatie');
 })
 
-app.get('/bekijken.html', (req: any, res: any) => {
-    res.render('bekijken', {dataBekijken: db_dataBekijken});
+app.get('/bekijken', (req: any, res: any) => {
+    db.query("select * from `Bekijken`", (err: any, results: any) => {
+        if (err) console.log("can't connect");
+        db_data = results;
+        let eersteId = db_data[0].fig_id;
+        eersteId = eersteId.toString();
+        for (var i = eersteId.length; i < 6; i++) {
+            eersteId = "0" + eersteId;
+        }
+        console.log(eersteId);
+        let fig = axios.get(`https://rebrickable.com/api/v3/lego/minifigs/fig-${eersteId}/?key=3ef36135e7fda4370a11fd6191fef2af`).
+            then(function (response: any) {
+                res.render('bekijken',{dataBekijken:response.data});
+                return response.data;
+            })
+        console.log("fig");
+        console.log(fig);
+        figdata = fig;
+        
+    })
+    
 })
 
-app.get('/blacklist.html', (req: any, res: any) => {
+app.get('/blacklist', (req: any, res: any) => {
     res.render('blacklist');
 })
 
-app.get('/ordenen.html', (req: any, res: any) => {
+app.get('/ordenen', (req: any, res: any) => {
     res.render('ordenen');
 })
 
