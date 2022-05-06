fdb19bdd8f5169446737a168ff6ff471aa1f8800
AlexanderBarbe <alexander.barbe@hotmail.com>
2022-05-06 11:05:04 +0200

    ordenen bezig

--- views/scripts/java.js
+++ views/scripts/java.js
@@ -1,4 +1,19 @@
 //minifigs
+const mysql = require('mysql');
+
+var db = mysql.createConnection({
+    host: 'db4free.net',
+    user: 'yaba_it_project',
+    password: 'yaba_it_project',
+    database: 'yaba_it_project'
+});
+
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Database is connected successfully');
+});
+
 console.log("java");
 let id = "fig-000051";
 fetch(`https://rebrickable.com/api/v3/lego/minifigs/${id}/?key=3ef36135e7fda4370a11fd6191fef2af`).
@@ -29,8 +44,8 @@ then(function (response) {
 
         
         let miniFigHtml = document.getElementById("miniFigs");
-        miniFigHtml.insertAdjacentHTML("beforeend", `<img src="${miniFigs.set_img_url}">`);
-        miniFigHtml.insertAdjacentHTML("beforeend", `<p class="naam">${miniFigs.name}</p>`);
+        miniFigHtml.insertAdjacentHTML("beforeend", `<td><img src="${miniFigs.set_img_url}"></td>`);
+        miniFigHtml.insertAdjacentHTML("beforeend", `<td><p class="naam">${miniFigs.name}</p></td>`);
 })
 
 fetch(`https://rebrickable.com/api/v3/lego/minifigs/${id}/sets/?key=3ef36135e7fda4370a11fd6191fef2af`)
@@ -46,16 +61,34 @@ fetch(`https://rebrickable.com/api/v3/lego/minifigs/${id}/sets/?key=3ef36135e7fd
 
         let sets = response.results;
         console.log("list: ");
-        console.log(sets[0]);
+        console.log(sets);
 
         
-        let miniFigHtml = document.getElementById("miniFigs");
+        let setsHtml = document.getElementById("figSets");
 
         for (let i = 0; i<sets.length;i++) {
-        miniFigHtml.insertAdjacentHTML("beforeend", `<img src="${sets[i].set_img_url}">`);
-        miniFigHtml.insertAdjacentHTML("beforeend", `<p class="naam">${sets[i].name}</p>`);
+            let id = sets[i].set_num.split("-");
+
+            console.log(id);
+        setsHtml.insertAdjacentHTML("beforeend", `<td><button onclick=ordenen(${i})><img src="${sets[i].set_img_url}></button></td`);
+        setsHtml.insertAdjacentHTML("beforeend", `<td><p class="naam">${sets[i].name}</p><p id="${i}">${sets[i].set_num}</p></td>`);
         }
+})
+
+const ordenen = (id) => {
+    let ordenenHtml = document.getElementById(id);
+    let url = ordenenHtml.innerHTML;
+    console.log(url);
+    console.log(id);
+    for (let teller = 1; teller<id.length; teller++) {
+        dezeId = " - " + id[teller]; 
+    }
+    db.query("select * from `Bekijken`", (err, results) => {
+        if (err) console.log("can't connect");
+        console.log(results);
     })
+
+}
 
 // //sets
 // fetch(`https://rebrickable.com/api/v3/lego/sets/?key=3ef36135e7fda4370a11fd6191fef2af`).
