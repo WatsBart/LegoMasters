b950e956a689d5f7f0a07bff938f5ef4ff7a45e5
Rishab Bhandari <rishab.bhandari@student.ap.be>
2022-05-13 11:04:54 +0200

    mongodb

--- express.ts
+++ express.ts
@@ -1,81 +1,53 @@
-//code afkomstig van het vak Webontwikkeling
+//code afkomstig van het vak Webprogrammeren
 
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
 const axios = require('axios');
-const mysql = require('mysql');
+
 app.set('view engine', 'ejs');
 app.set('port', 3000);
 app.use(express.static('views'));
 
 
-var db_data: any;
-var figdata: any;
+const {MongoClient} = require('mongodb');
+const uri = "mongodb+srv://yaba:yabaitproject@cluster0.bj6tu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+let Main = async () => {
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        //...
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+Main();
 
-var db = mysql.createConnection({
-    host: 'db4free.net',
-    user: 'yaba_it_project',
-    password: 'yaba_it_project',
-    database: 'yaba_it_project'
-});
 
-
-db.connect((err: any) => {
-    if (err) throw err;
-    console.log('Database is connected successfully');
-});
-
-app.get('/', (req: any, res: any) => {
+app.get('/index.html', (req: any, res: any) => {
     res.render('index')
 });
 
-app.get('/index', (req: any, res: any) => {
+app.get('/index.html', (req: any, res: any) => {
     res.render('index');
 })
 
-app.get('/informatie', (req: any, res: any) => {
-    res.render('informatie');
+app.get('/informatie.html', (req: any, res: any) => {
+    res.render('informatie'/*, { data: db_data }*/);
 })
 
-app.get('/bekijken', (req: any, res: any) => {
-    db.query("select * from `Bekijken`", (err: any, results: any) => {
-        if (err) console.log("can't connect");
-        db_data = results;
-        let eersteId = db_data[0].fig_id;
-        eersteId = eersteId.toString();
-        for (var i = eersteId.length; i < 6; i++) {
-            eersteId = "0" + eersteId;
-        }
-        console.log(eersteId);
-        let fig = axios.get(`https://rebrickable.com/api/v3/lego/minifigs/fig-${eersteId}/?key=3ef36135e7fda4370a11fd6191fef2af`).
-            then(function (response: any) {
-                res.render('bekijken',{dataBekijken:response.data});
-                return response.data;
-            })
-        console.log("fig");
-        console.log(fig);
-        figdata = fig;
-        
-    })
+app.get('/contact.html', (req: any, res: any) => {
+    res.render('contact');
 })
 
-app.get('/blacklist', (req: any, res: any) => {
-    res.render('blacklist');
-})
-
-app.get('/ordenen', (req: any, res: any) => {
-    res.render('ordenen');
-})
-
-app.get('/databaseInsert', (req: any, res: any) => {
-    console.log(req.query);
-    console.log("get request aanvaard");
-    let waarden = req.query;
-    db.query(`insert into Bekijken (user_id, fig_id, set_id) values(1, "${req.query.figId}", "${req.query.setId}");`, (err: any, results: any) => {
-        if (err) console.log(err);    
-    })
-})
 
 app.listen(app.get('port'),
     () => console.log('[server] http://localhost:' + app.get('port')));
+
+
