e2f59c42822572a51466587683d10a6cd69fd316
AlexanderBarbe <alexander.barbe@hotmail.com>
2022-05-20 08:23:32 +0200

    nieuwe database

--- express.ts
+++ express.ts
@@ -8,8 +8,6 @@ const axios = require('axios');
 app.set('view engine', 'ejs');
 app.set('port', 3000);
 app.use(express.static('views'));
-app.use(express.json({limit: '1mb'}));
-app.use(express.urlencoded({extended:true}));
 
 
 const {MongoClient} = require('mongodb');
@@ -44,36 +42,27 @@ app.get('/informatie.html', (req: any, res: any) => {
     res.render('informatie'/*, { data: db_data }*/);
 })
 
-app.get('/contact.html', (req: any, res: any) => {
-    res.render('contact');
-})
-
-<<<<<<< HEAD
-app.get('/blacklist', (req: any, res: any) => {
-    res.render('blacklist');
-})
-
 app.get('/ordenen', (req: any, res: any) => {
     res.render('ordenen');
-})
-
-app.post('/ordenen', (req:any, res:any) => {
-    console.log("post success");
-    console.log(req.body);
-    console.log(req.query);
-    console.log(req.params);
 })
 
 app.get('/databaseInsert', (req: any, res: any) => {
     console.log(req.query);
     console.log("get request aanvaard");
     let waarden = req.query;
-    db.query(`insert into Bekijken (user_id, fig_id, set_id) values(1, "${req.query.figId}", "${req.query.setId}");`, (err: any, results: any) => {
-        if (err) console.log(err);    
-    })
+    let insert = async () => {
+        try {
+            // Connect to the MongoDB cluster
+            await client.connect();
+            const result = await client.db('itproject').collection('yaba').insertOne({waarden});
+        } catch (e) {
+            console.error(e);
+        } finally {
+            await client.close();
+        }
+    }
+    insert();
 })
-=======
->>>>>>> a44e75c006f75e6bf3c4912c5b78bb447360dd0a
 
 app.listen(app.get('port'),
     () => console.log('[server] http://localhost:' + app.get('port')));
