3173e451dad95ca8253a62502b49c50cd7f0b607
AlexanderBarbe <alexander.barbe@hotmail.com>
2022-05-20 09:12:49 +0200

    cleanup

--- views/scripts/java.js
+++ views/scripts/java.js
@@ -4,16 +4,8 @@ console.log("java");
 let minifigs;
 
 const aantalKiezenHtml = document.getElementById("aantal");
-aantalKiezenHtml.insertAdjacentHTML("beforeend", '<input type="number" name="aantal" id="aantalKiezen" min="0"> <button type="button" onclick=aantalKiezen()>Submit</button>');
+aantalKiezenHtml.insertAdjacentHTML("beforeend", '<input type="number" name="aantal" id="aantalKiezen" min="1"> <button type="button" onclick=aantalKiezen()>Submit</button>');
 let aantalFigs;
-
-// let random = Math.floor(Math.random() * 10999);
-// console.log(random);
-// let id = random.toString();
-// for (var i = id.length; i < 6; i++) {
-//     id = "0" + id;
-// }
-// console.log(id);
 
 fetch(`https://rebrickable.com/api/v3/lego/minifigs/?key=3ef36135e7fda4370a11fd6191fef2af`).
 then(function (response) {
@@ -27,26 +19,9 @@ then(function (response) {
         console.log("fig");
         miniFigs = response.results;
         console.log(miniFigs);
-        // let figList = [];
-        // let numberarray = [];
-        // for(let i = 0; i < 6; i++){
-        //     randomgetal = Math.floor(Math.random()*miniFigs.length);
-        //     if(!numberarray.includes(randomgetal)){
-        //         numberarray.push(randomgetal);
-        //     }
-        // }
-
-        // for (let i = 0; i < 6; i++) {
-        //     figList[i] = miniFigs[numberarray[i]-1];
-        // }
 })
 
-
 //code afkomstig van w3schools
-
-// function showCustomer(str) {
-
-// }
 
 const tonen = () => {
     console.log("tonen");
@@ -64,54 +39,33 @@ fetch(`https://rebrickable.com/api/v3/lego/minifigs/${miniFigs[random].set_num}/
     }
 })
 .then(function(response){
-
-        let sets = response.results;
-        console.log("list: ");
-        console.log(sets);
-
-        
-        let setsHtml = document.getElementById("figSets");
-
-        for (let i = 0; i<sets.length;i++) {
-            let id = sets[i].set_num.split("-");
-
-            console.log(id);
+    let sets = response.results;    
+    let setsHtml = document.getElementById("figSets");
+    for (let i = 0; i<sets.length;i++) {
+        let id = sets[i].set_num.split("-");
         setsHtml.insertAdjacentHTML("beforeend", `<td><button onclick=ordenen(${i})><img src="${sets[i].set_img_url}"></button></td`);
         setsHtml.insertAdjacentHTML("beforeend", `<td><p class="naam">${sets[i].name}</p><p id="${i}">${sets[i].set_num}</p></td>`);
-        }
+    }
 })
 
 }
 
 const aantalKiezen = () => {
-    console.log("aantal kiezen");
     aantalFigs = parseInt(document.getElementById("aantalKiezen").value) - 1;
     console.log(aantalFigs);
     document.getElementById("aantal").innerHTML = "";
     tonen();
 }
 
-
 const ordenen = (id) => {
-    console.log("ordenen");
     let ordenenHtml = document.getElementById(id);
     let setId = ordenenHtml.innerHTML;
     
     let ordenenHtmlFig = document.getElementById("figId");
     let figId = ordenenHtmlFig.innerHTML;
-    // console.log(url);
-    // console.log(id);
-    // for (let teller = 1; teller<id.length; teller++) {
-    //     dezeId = " - " + id[teller]; 
-    // }
-    // db.query("select * from `Bekijken`", (err, results) => {
-    //     if (err) console.log("can't connect");
-    //     console.log(results);
-    // })
 
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
-        console.log('hallo');
     }
     xhttp.open("GET", "databaseInsert?figId="+figId + "&setId="+setId);
     xhttp.send();
@@ -120,6 +74,8 @@ const ordenen = (id) => {
     if (aantalFigs > 0) {
         aantalFigs--;
         tonen();
+    } else {
+        aantalKiezenHtml.insertAdjacentHTML("beforeend", '<input type="number" name="aantal" id="aantalKiezen" min="1"> <button type="button" onclick=aantalKiezen()>Submit</button>');
     }
 }
 
@@ -145,36 +101,3 @@ const blacklistFig = () => {
     }
     xhttp.send(params);
 }
-
-// //sets
-// fetch(`https://rebrickable.com/api/v3/lego/sets/?key=3ef36135e7fda4370a11fd6191fef2af`).
-// then(function (response) {
-//     if (response.ok) {
-//         return response.json();
-//     }else {
-//         return Promise.reject(response.status);
-//     }
-// }).then(function (response) {
-//         console.log(response);
-//         let figSets = response.results;
-//         let setList = [];
-
-//         let numberarray = [];
-//         for(let i = 0; i < 6; i++){
-//             randomgetal = Math.floor(Math.random()*figSets.length);
-//             if(!numberarray.includes(randomgetal)){
-//                 numberarray.push(randomgetal);
-//             }
-//         }
-
-//         for (let i = 0; i < 6; i++) {
-//             setList[i] = figSets[numberarray[i]-1];
-//         }
-
-//     console.log(setList);
-//     let figSetHtml = document.getElementById("figSets");
-//     for (let i = 0; i<setList.length;i++) {
-//         figSetHtml.insertAdjacentHTML("beforeend", `<img src="${setList[i].set_img_url}">`);
-//         figSetHtml.insertAdjacentHTML("beforeend", `<p class="naam">${setList[i].name}</p>`);
-//     }
-// })
